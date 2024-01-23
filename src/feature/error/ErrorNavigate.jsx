5984d76a3904964937e3b3a9b573553884df6459
suvannapoom <suvannapoom.bo@ku.th>
2024-01-23 14:21:10 +0700

    ss

--- src/feature/error/ErrorNavigate.jsx
+++ src/feature/error/ErrorNavigate.jsx
@@ -3,11 +3,11 @@ import Loading from "../../layouts/Loading";
 
 export default function ErrorNavigate() {
   useEffect(() => {
-    // window.parent.location.href = "https://ai-card-q.vercel.app/errorpage";
+    window.parent.location.href = "https://ai-card-q.vercel.app/errorpage";
     //   return () => {
     //     second
     //   }
   }, []);
 
-  return <div className=""></div>;
+  return <div className="w-screen h-screen bg-white"></div>;
 }

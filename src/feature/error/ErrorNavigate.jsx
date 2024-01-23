f12127e31d753e77605ef6e45f81e50164563298
suvannapoom <suvannapoom.bo@ku.th>
2024-01-23 13:44:53 +0700

    test

--- src/feature/error/ErrorNavigate.jsx
+++ src/feature/error/ErrorNavigate.jsx
@@ -3,11 +3,10 @@ import React, { useEffect } from "react";
 export default function ErrorNavigate() {
   useEffect(() => {
     window.parent.location.href = "https://ai-card-q.vercel.app/errorpage";
-
     //   return () => {
     //     second
     //   }
-  }, [third]);
+  }, []);
 
   return <div>Yoooooooooooooooooooooooooooooooooooooooooooooooooo</div>;
 }

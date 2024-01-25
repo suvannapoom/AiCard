7df1c65ee6102b3af6a872d1149bb17cdafd863a
suvannapoom <suvannapoom.bo@ku.th>
2024-01-25 13:11:54 +0700

    ssssSSS

--- src/feature/error/ErrorNavigate.jsx
+++ src/feature/error/ErrorNavigate.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 
 export default function ErrorNavigate() {
   useEffect(() => {
-    // window.parent.location.href = "https://ai-card-q.vercel.app/";
+    window.parent.location.href = "https://ai-card-q.vercel.app/errorpage";
   }, []);
 
   return <div className="w-screen h-screen bg-white"></div>;
